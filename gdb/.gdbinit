3299ea996ee3f239ac21f213be01c3ae1063b61a
tylerwarre <tyler@warrens.one>
2026-05-12 16:26:39 -0500

    Added configurations for bash and gdb

---
+++ gdb/.gdbinit
@@ -0,0 +1,2 @@
+source ~/.gef-2026.01.py
+set disassembly-flavor intel
--- install.sh
+++ install.sh
@@ -5,8 +5,15 @@ cp -r ./vim ~/.vim
 
 # Install tmux
 cp -r ./tmux ~/.tmux
-mv ~/.tmux/.tmux.conf ~/
+cp ~/.tmux/.tmux.conf ~/
 
 # Install git
 cp ./git/.gitconfig ~/
 git clone https://github.com/tylerwarre/htb.git
+mv htb ~/
+
+# Install gdb
+cp ./gdb/* ~/
+
+# Install bash
+cat ./bash/.bashrc >> ~/.bashrc
