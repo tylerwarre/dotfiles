a299a2d71d41b51b0dbec29a98743c3ecdc67674
tylerwarre <tyler@warrens.one>
2026-07-02 13:26:33 -0400

    Added nvim install

--- install.sh
+++ install.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+# Get github token
+read -sp "test: " token
+
 # Install vim
 cp -r ./vim ~/.vim
+
+# Install neovim
+curl -L -O https://github.com/neovim/neovim/releases/download/v0.12.3/nvim-linux-x86_64.tar.gz
+tar xf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
+sudo mv nvim-linux-x86_64/ /opt/nvim/
+sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/
+
+# Configure neovim
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+rm -r /usr/local/bin/nvim
+git clone --depth 1 https://github.com/tylerwarre/nvim.git ~/.config/nvim
+xargs -a ~/.config/nvim/pkglist.txt sudo apt install -y
 
 # Install tmux
 cp -r ./tmux ~/.tmux
@@ -11,11 +28,12 @@ cp ~/.tmux/.tmux.conf ~/
 cp ./git/.gitconfig ~/
 
 # Fetch HTB Notes
-git clone --depth 1 https://github.com/tylerwarre/htb.git
+echo "https://tylerwarre:$token@github.com/tylerwarre/htb.git"
+git clone --depth 1 "https://tylerwarre:$token@github.com/tylerwarre/htb.git"
 mv htb ~/
 
 # Fetch Wiki
-git clone --depth 1 https://github.com/tylerwarre/wiki.git
+git clone --depth 1 "https://tylerwarre:$token@github.com/tylerwarre/wiki.git"
 mv wiki ~/
 
 # Install gdb
