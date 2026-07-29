46bb3ba286f4e0caba6a619cad37f42245af9a3b
tylerwarre <tyler@warrens.one>
2026-07-29 17:31:00 -0400

    Modularized all tasks and stated implementing --debug

--- install.sh
+++ install.sh
@@ -1,66 +1,128 @@
 #!/bin/bash
 
-function get_options {
-
+get_options() {
+	if [[ -n $GH_TOKEN ]]; then
+		token=$GH_TOKEN
+	else
+		read -sp "GH_TOKEN: " token
+		echo ""
+	fi
+	read -p "Install vpn? (y/n): " do_vpn
 }
 
-# Get github token
-read -sp "test: " token
-read -p "Install vpn? (y/n): " do_vpn
+install_bash() {
+	local err=0
+	printf "\r[?] Installing bash"
 
-# Install fonts
-#sudo cp fonts/* /usr/local/share/fonts/
+	{
+		cp ./bash/.env ~/.env
+		echo -e "\nexport GH_TOKEN=$token" >> ~/.env
+		err=$((err + $?))
 
-# Install mate-terminal
-dconf load /org/mate/terminal/ < mate-terminal/mate-terminal-backup.txt
+		if [[ ! -f ~/.bashrc.bak ]]; then
+			cp ~/.bashrc ~/.bashrc.bak
+		fi
+		cp ~/.bashrc.bak ~/.bashrc
+		err=$((err + $?))
 
-# Install vim
-cp -r ./vim ~/.vim
+		echo -e "\nsource ~/.env" >> ~/.bashrc
+		err=$((err + $?))
 
-# Install neovim
-curl -L -O https://github.com/neovim/neovim/releases/download/v0.12.4/nvim-linux-x86_64.tar.gz
-tar xf nvim-linux-x86_64.tar.gz
-rm nvim-linux-x86_64.tar.gz
-sudo mv nvim-linux-x86_64/ /opt/nvim/
-sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/
+		source ~/.env
+		err=$((err + $?))
+	}
 
-# Configure neovim
-rm -rf ~/.config/nvim
-rm -rf ~/.local/share/nvim
-rm -r /usr/local/bin/nvim
-git clone --depth 1 --branch v0.12.4 https://github.com/tylerwarre/nvim.git ~/.config/nvim
-xargs -a ~/.config/nvim/pkglist.txt sudo apt install -y
+	if [[ $err -eq 0 ]]; then
+		printf "\r[+] Installing bash\n"
+	else
+		printf "\r[-] Installing bash\n"
+	fi
+}
 
-# Install tmux
-cp -r ./tmux ~/.tmux
-cp ~/.tmux/.tmux.conf ~/
+install_fonts() {
+	sudo cp fonts/* /usr/local/share/fonts/
+}
 
-# Install git
-cp ./git/.gitconfig ~/
+install_mater-terminal() {
+	dconf load /org/mate/terminal/ < mate-terminal/mate-terminal-backup.txt
+}
 
-# Fetch HTB Notes
-echo "https://tylerwarre:$token@github.com/tylerwarre/htb.git"
-git clone --depth 1 "https://tylerwarre:$token@github.com/tylerwarre/htb.git"
-mv htb ~/
+install_vim() {
+	cp -r ./vim ~/.vim
+}
 
-# Fetch Wiki
-git clone --depth 1 "https://tylerwarre:$token@github.com/tylerwarre/wiki.git"
-mv wiki ~/
+configure_nvim() {
+	rm -rf ~/.config/nvim
+	rm -rf ~/.local/share/nvim
+	rm -r /usr/local/bin/nvim
+	git clone --depth 1 --branch v0.12.4 https://github.com/tylerwarre/nvim.git ~/.config/nvim
+	xargs -a ~/.config/nvim/pkglist.txt sudo apt install -y
+}
 
-# Install gdb
-cp ./gdb/* ~/
+install_nvim() {
+	curl -L -O https://github.com/neovim/neovim/releases/download/v0.12.4/nvim-linux-x86_64.tar.gz
+	tar xf nvim-linux-x86_64.tar.gz
+	rm nvim-linux-x86_64.tar.gz
+	sudo mv nvim-linux-x86_64/ /opt/nvim/
+	sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/
 
-# Init VPN
-if [[ "$do_vpn" == "y" ]]; then
-  sudo systemctl stop openvpn@$(whoami)
-  sudo mv ~/lab-vpn.conf /etc/openvpn/client/
-  sudo systemctl start openvpn-client@lab-vpn
+	configure_nvim
+}
+
+install_tmux() {
+	cp -r ./tmux ~/.tmux
+	cp ~/.tmux/.tmux.conf ~/
+}
+
+install_git() {
+	cp ./git/.gitconfig ~/
+}
+
+install_gdb() {
+	cp ./gdb/* ~/
+}
+
+configure_pwnbox-timer() {
+	startup=$(loginctl session-status | grep -Po "(?<=Since: [A-Za-z]{3} \d{4}-\d{2}-\d{2} \d{2}:)\d+(?=:\d+)" | sed "s/^0*//")
+	(crontab -l 2>/dev/null; echo "$((startup + 10)) * * * * /usr/bin/notify-send -u critical -t 25000 'Pwnbox' 'Refresh your session'") | crontab -
+}
+
+get_htb-notes() {
+	echo "https://tylerwarre:$token@github.com/tylerwarre/htb.git"
+	git clone --depth 1 "https://tylerwarre:$token@github.com/tylerwarre/htb.git"
+	mv htb ~/
+}
+
+get_wiki() {
+	git clone --depth 1 "https://tylerwarre:$token@github.com/tylerwarre/wiki.git"
+	mv wiki ~/
+}
+
+install_vpn() {
+	if [[ "$do_vpn" == "y" ]]; then
+		sudo systemctl stop openvpn@$(whoami)
+		sudo mv ~/lab-vpn.conf /etc/openvpn/client/
+		sudo systemctl start openvpn-client@lab-vpn
+	fi
+}
+
+# TODO: Change to use custome file descriptor for grouped commands instead
+if [[ "$1" == "--debug" ]]; then
+	output=""
+else
+	output="> /dev/null 2>&1"
 fi
 
-# Install bash
-echo "" >> ~/.bashrc
-cat ./bash/.bashrc >> ~/.bashrc
-
-# TMP
-# Get Startup time: startup=$(loginctl session-status | grep -Po "(?<=Since: [A-Za-z]{3} \d{4}-\d{2}-\d{2} \d{2}:)\d+(?=:\d+)" | sed "s/^0*//")
-# Install crontab: (crontab -l 2>/dev/null; echo "$((startup + 10)) * * * * /usr/bin/notify-send -u critical -t 25000 'Pwnbox' 'Refresh your session'") | crontab -
+get_options
+install_bash
+get_htb-notes
+get_wiki
+configure_pwnbox-timer
+install_fonts
+install_mate-terminal
+install_tmux
+install_vim
+install_gdb
+install_git
+install_nvim
+install_vpn
